60c740b5733ac8c6f46e4a9ab6299bad1b8f3198
davor <davorzec.emedia@gmail.com>
2023-09-04 19:04:17 +0200

    Dorade na DPD rjesio error handling i slanje polja

--- app/Http/Controllers/Api/Wordpress/HR/DpdController.php
+++ app/Http/Controllers/Api/Wordpress/HR/DpdController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Response;
 
 class DpdController extends Controller
 {
@@ -17,33 +16,58 @@ class DpdController extends Controller
 
         if (!isset($jsonData->user, $jsonData->parcel)) {
             return response()->json([
-                "status" => 400,
-                "info" => "Bad Request",
-                "data" => "Missing 'user' and/or 'parcel' properties in JSON data"
+                "errors" => [
+                    [
+                        "status" => 400,
+                        "id" => 123456,
+                        "type_id" => "Dodati vrste errora",
+                        "courier_id" => "DPD",
+                        "title" =>  "Invalid Attribute",
+                        "detail" => "Missing user property."
+                    ]
+                ],
             ], 400);
         }
 
         $user = $jsonData->user;
         $parcel = $jsonData->parcel;
 
-        $parcel_response = Http::post(config('urls.hr.dpd') . '/parcel/parcel_import?' .
-            "username=$user->username&" .
-            "password=$user->password&" .
-            "cod_amount=$parcel->cod_amount&" .
-            "name1=$parcel->name1&" .
-            "street=$parcel->street&" .
-            "rPropNum=$parcel->rPropNum&" .
-            "city=$parcel->city&" .
-            "country=$parcel->country&" .
-            "pcode=$parcel->pcode&" .
-            "email=$parcel->email&" .
-            "sender_remark=$parcel->sender_remark&" .
-            "weight=$parcel->weight&" .
-            "order_number=$parcel->order_number&" .
-            "cod_purpose=$parcel->cod_purpose&" .
-            "parcel_type=$parcel->parcel_type&" .
-            "num_of_parcel=$parcel->num_of_parcel");
+        $parcel_response = Http::post(config('urls.hr.dpd') .
+            '/parcel/parcel_import?' .
+            "username=$user->username&password=$user->password&" .
+            http_build_query($parcel));
+
         $parcel_response_json = json_decode($parcel_response->body());
+
+        if ($parcel_response->successful()) {
+            if ($parcel_response_json->status === 'err') {
+                return response()->json([
+                    "errors" => [
+                        [
+                            "status" => 1,
+                            "id" => 123456,
+                            "type_id" => "Dodati vrste errora",
+                            "courier_id" => "DPD",
+                            "title" =>  $parcel_response_json->status,
+                            "detail" => $parcel_response_json->errlog
+                        ]
+                    ],
+                ], 400);
+            }
+        } else {
+            return response()->json([
+                "errors" => [
+                    [
+                        "status" => $parcel_response->status(),
+                        "id" => 123456,
+                        "type_id" => "Dodati vrste errora",
+                        "courier_id" => "DPD",
+                        "title" =>  "DPD error",
+                        "detail" => "DPD server je vratio grešku."
+                    ]
+                ]
+            ], $parcel_response->status());
+        }
 
         $pl_numbers = implode(",", $parcel_response_json->pl_number);
 
@@ -52,29 +76,47 @@ class DpdController extends Controller
                 'responseType' => 'blob'
             ],
             "content-type" => "application/x-www-form-urlencoded"
-        ])->post(config('urls.hr.dpd') . '/parcel/parcel_print?' .
-            "username=$user->username&" .
-            "password=$user->password&" .
+        ])->post(config('urls.hr.dpd') .
+            '/parcel/parcel_print?' .
+            "username=$user->username&password=$user->password&" .
             "parcels=$pl_numbers");
 
         if ($parcel_label_response->successful()) {
-            $response = [
-                "status" => 200,
-                "info" => "Success",
-                "data" => [
-                    "parcels" => $pl_numbers,
-                    "labels" => base64_encode($parcel_label_response->body()) // Encode as base64 for JSON
-                ]
-            ];
-
-            return response()->json($response, 200);
+            if ($parcel_response_json->status === 'err') {
+                return response()->json([
+                    "errors" => [
+                        [
+                            "status" => 2,
+                            "id" => 123456,
+                            "type_id" => "Dodati vrste errora",
+                            "courier_id" => "DPD",
+                            "title" =>  $parcel_response_json->status,
+                            "detail" => $parcel_response_json->errlog
+                        ]
+                    ],
+                ], 400);
+            }
         } else {
             return response()->json([
-                "status" => $parcel_label_response->status(),
-                "info" => $parcel_label_response->reason(),
-                "data" => []
+                "errors" => [
+                    [
+                        "status" => $parcel_label_response->status(),
+                        "id" => 123456,
+                        "type_id" => "Dodati vrste errora",
+                        "courier_id" => "DPD",
+                        "title" =>  "DPD error",
+                        "detail" => "DPD server je vratio grešku."
+                    ]
+                ]
             ], $parcel_label_response->status());
         }
+
+        return response()->json([
+            "data" => [
+                "parcels" => $pl_numbers,
+                "labels" => base64_encode($parcel_label_response->body()) // Encode as base64 for JSON
+            ]
+        ], 201);
     }
 
     public function printLabels()
