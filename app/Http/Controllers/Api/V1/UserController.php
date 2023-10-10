588e6fd3e2d48fb958895b8e0e53d88faa394e76
davor <davorzec.emedia@gmail.com>
2023-10-10 23:03:09 +0200

    .

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -17,22 +17,13 @@ class UserController extends Controller
         $requestBody = $request->getContent();
         $data = json_decode($requestBody);
 
-        if (!User::where('email', $data->email)->exists() && empty(UserService::getWpUser($data->email))) {
-            User::create([
-                'email' => $data->email,
-                'wp_user_id' => $data->wp_user_id
-            ]);
+        User::create([
+            'email' => $data->email,
+            'wp_user_id' => $data->wp_user_id
+        ]);
 
-            return response()->json([
-                "data" => "User created",
-            ], 201);
-        } else {
-            return response()->json([
-                "errors" => [
-                    "error"
-                    //ErrorService::write("", 403, "Could not create user.", $data, "App\Http\Controllers\Api\V1\UserController@create" . __LINE__, ''),
-                ],
-            ], 403);
-        }
+        return response()->json([
+            "data" => "User created",
+        ], 201);
     }
 }
