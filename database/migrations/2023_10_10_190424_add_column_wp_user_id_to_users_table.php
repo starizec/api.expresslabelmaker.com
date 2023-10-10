b46d1e1cd8f03695dfc05ed5273c964a31c0e6b7
davor <davorzec.emedia@gmail.com>
2023-10-10 21:52:03 +0200

    Dodao nullable

--- database/migrations/2023_10_10_190424_add_column_wp_user_id_to_users_table.php
+++ database/migrations/2023_10_10_190424_add_column_wp_user_id_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('wp_user_id')->after('id')->unique();
+            $table->integer('wp_user_id')->after('id')->unique()->nullable();
         });
     }
 
