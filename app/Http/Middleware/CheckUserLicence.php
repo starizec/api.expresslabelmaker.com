ad580e3a2080e8595d46eae4f1a91a28039743b3
davor <davorzec.emedia@gmail.com>
2024-04-02 20:04:33 +0200

    a

--- app/Http/Middleware/CheckUserLicence.php
+++ app/Http/Middleware/CheckUserLicence.php
@@ -46,6 +46,7 @@ class CheckUserLicence
 
             $user_s = new UserService();
             $licence = $user_s->checkUserLicence(new UserClass($jsonData['user']['email'], DomainService::parseDomain($jsonData['user']['domain']), $jsonData['user']['licence']), $pl_no);
+            return response()->json($licence);
 
             if ($licence['status'] > 300) {
                 return response()->json([
