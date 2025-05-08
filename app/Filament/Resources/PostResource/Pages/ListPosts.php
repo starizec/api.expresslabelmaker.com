0d7f65dd5bc0dc0f21490166c84637d825623bde
Davor <davor@emedia.hr>
2025-05-08 20:45:37 +0200

    PostResource

--- app/Filament/Resources/PostResource/Pages/ListPosts.php
+++ app/Filament/Resources/PostResource/Pages/ListPosts.php
@@ -4,8 +4,16 @@ namespace App\Filament\Resources\PostResource\Pages;
 
 use App\Filament\Resources\PostResource;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Actions;
 
 class ListPosts extends ListRecords
 {
     protected static string $resource = PostResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
 } 
