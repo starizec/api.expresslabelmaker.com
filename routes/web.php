7cbc1ada274ca77199cf51b1acd1d2d56c8c0457
Davor <davor@emedia.hr>
2025-05-15 20:21:05 +0200

    getdeliverylocations

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('language/{lang}', [App\Http\Controllers\LanguageController::class, 'switchLang'])->name('language.switch');
 
-// Handle root URL directly
 Route::get('/', [App\Http\Controllers\PageController::class, 'home'])->defaults('lang', 'hr');
 
 Route::group(['prefix' => '{lang}', 'where' => ['lang' => '[a-zA-Z]{2}'], 'defaults' => ['lang' => 'hr']], function () {
@@ -28,18 +27,18 @@ Route::group(['prefix' => '{lang}', 'where' => ['lang' => '[a-zA-Z]{2}'], 'defau
         Route::post('/password/reset', [\App\Http\Controllers\Auth\ResetPasswordController::class, 'reset'])->name('password.update');
     });
 
-    Route::middleware(['auth', 'admin'])->group(function () {
-        Route::prefix('admin')->group(function () {
-            Route::get('overseas/delivery-locations', [App\Http\Controllers\DeliveryLocations\HR\OverseasController::class, 'getDeliveryLocations'])->name('overseas-delivery-locations');
-            Route::get('dpd/delivery-locations', [App\Http\Controllers\DeliveryLocations\HR\DpdController::class, 'getDeliveryLocations'])->name('dpd-delivery-locations');
-            Route::get('hp/delivery-locations', [App\Http\Controllers\DeliveryLocations\HR\HpController::class, 'getDeliveryLocations'])->name('hp-delivery-locations');
-        });
-    });
-
     Route::middleware('auth')->group(function () {
         Route::get('/profile', [App\Http\Controllers\UserProfileController::class, 'index'])->name('profile');
         Route::put('/profile/update', [App\Http\Controllers\UserProfileController::class, 'update'])->name('profile.update');
         Route::post('/logout', [App\Http\Controllers\Auth\LogoutController::class, 'logout'])->name('logout');
+    });
+});
+
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::prefix('admin')->group(function () {
+        Route::get('overseas/delivery-locations', [App\Http\Controllers\DeliveryLocations\HR\OverseasController::class, 'getDeliveryLocations'])->name('overseas-delivery-locations');
+        Route::get('dpd/delivery-locations', [App\Http\Controllers\DeliveryLocations\HR\DpdController::class, 'getDeliveryLocations'])->name('dpd-delivery-locations');
+        Route::get('hp/delivery-locations', [App\Http\Controllers\DeliveryLocations\HR\HpController::class, 'getDeliveryLocations'])->name('hp-delivery-locations');
     });
 });
 
