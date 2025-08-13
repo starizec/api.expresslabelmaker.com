375771d71072f84459cad307035b04ee62ebea00
Davor <davor@emedia.hr>
2025-08-13 21:31:10 +0200

    dpd nova polja i statusi

--- app/Http/Controllers/Api/V1/HR/DpdController.php
+++ app/Http/Controllers/Api/V1/HR/DpdController.php
@@ -11,7 +11,7 @@ use Illuminate\Validation\ValidationException;
 use App\Services\UserService;
 use App\Services\Logger\ApiErrorLogger;
 use App\Services\Logger\ApiUsageLogger;
-
+use App\Services\AdressService;
 use App\Models\DeliveryLocationHeader;
 use App\Models\DeliveryLocation;
 use App\Models\Courier;
@@ -74,8 +74,15 @@ class DpdController extends Controller
                 : $parcelResponse->status() . " - DPD Server error";
 
             ApiErrorLogger::apiError(
-                $this->courier->country->short . ' - ' . $this->courier->name . ' - ' . $user->domain . ' - ' . $error_message,
+                $this->courier->country->short . ' - ' . $this->courier->name . ' - ' . $user->domain . ' - ' . $error_message . ' - Client',
                 $request,
+                $error_message,
+                __CLASS__ . '@' . __FUNCTION__ . '::' . __LINE__
+            );
+
+            ApiErrorLogger::apiError(
+                $this->courier->country->short . ' - ' . $this->courier->name . ' - ' . $user->domain . ' - ' . $error_message . ' - Server',
+                $this->prepareParcelPayload($parcel),
                 $error_message,
                 __CLASS__ . '@' . __FUNCTION__ . '::' . __LINE__
             );
@@ -188,8 +195,15 @@ class DpdController extends Controller
                     : $parcelResponse->status() . " - DPD Server error";
 
                 ApiErrorLogger::apiError(
-                    $this->courier->country->short . ' - ' . $this->courier->name . ' - ' . $user->domain . ' - ' . $error_message,
+                    $this->courier->country->short . ' - ' . $this->courier->name . ' - ' . $user->domain . ' - ' . $error_message . ' - Client',
                     $request,
+                    $error_message,
+                    __CLASS__ . '@' . __FUNCTION__ . '::' . __LINE__
+                );
+
+                ApiErrorLogger::apiError(
+                    $this->courier->country->short . ' - ' . $this->courier->name . ' - ' . $user->domain . ' - ' . $error_message . ' - Server',
+                    $this->prepareParcelPayload($parcel->parcel),
                     $error_message,
                     __CLASS__ . '@' . __FUNCTION__ . '::' . __LINE__
                 );
@@ -287,8 +301,9 @@ class DpdController extends Controller
         }
 
         ApiUsageLogger::apiUsage(
-            $this->courier->country->short . ' - ' . $this->courier->name . ' - ' . $user->domain, 
-            $request);
+            $this->courier->country->short . ' - ' . $this->courier->name . ' - ' . $user->domain,
+            $request
+        );
 
         return response()->json([
             "data" => [
@@ -329,6 +344,8 @@ class DpdController extends Controller
                 $error_message,
                 __CLASS__ . '@' . __FUNCTION__ . '::' . __LINE__
             );
+
+
 
             return response()->json([
                 "errors" => [
@@ -391,98 +408,152 @@ class DpdController extends Controller
 
     protected function prepareParcelPayload($parcel)
     {
-        return [
-            "name1" => $parcel->name1,
-            "street" => $parcel->street,
-            "rPropNum" => $parcel->rPropNum,
-            "city" => $parcel->city,
-            "country" => strtoupper($this->courier->country->short),
-            "pcode" => $parcel->pcode,
-            "email" => $parcel->email ?? null,
-            "phone" => $parcel->phone ?? null,
-            "contact" => $parcel->contact ?? null,
-            "sender_remark" => $parcel->sender_remark ?? null,
-            "weight" => !empty($parcel->weight) ? (float) $parcel->weight : null,
-            "num_of_parcel" => (int) $parcel->num_of_parcel,
-            "order_number" => $parcel->order_number ?? null,
-            "parcel_type" => $parcel->parcel_type,
-            "cod_amount" => !empty($parcel->cod_amount) ? (float) $parcel->cod_amount : null,
-            "cod_purpose" => $parcel->cod_purpose ?? null,
-            "pudo_id" => $parcel->pudo_id ?? null,
-        ];
-    }
+        $recipient_adress = AdressService::splitAddress($parcel->recipient_adress);
 
+        $additionalServicesIds = explode(',', $parcel->additional_services);
+
+        switch ($parcel->delivery_sevice) {
+            case "B2C":
+                if ($parcel->cod_amount > 0) {
+                    $delivery_service = "D-COD-B2C";
+                } else if (isset($parcel->location_id)) {
+                    $delivery_service = "D-B2C-PSD";
+                } else {
+                    $delivery_service = "D-B2C";
+                }
+                break;
+            case "B2B":
+                if ($parcel->cod_amount > 0) {
+                    $delivery_service = "D-COD";
+                } else {
+                    $delivery_service = "D";
+                }
+                break;
+            case "TYRE":
+                $delivery_service = "D-TYRE";
+                break;
+            case "TYRE-B2C":
+                $delivery_service = "D-TYRE-B2C";
+                break;
+            case "PAL":
+                $delivery_service = "PAL";
+                break;
+            case "SWAP":
+                $delivery_service = "D-SWAP";
+                break;
+        }
+
+        $location = DeliveryLocation::where('id', $parcel->location_id)->latest()->first();
+
+        $payload = [
+            "name1" => $parcel->recipient_name,
+            "street" => $recipient_adress['street'],
+            "rPropNum" => $recipient_adress['house_number'],
+            "city" => $parcel->recipient_city,
+            "country" => strtoupper($this->courier->country->short),
+            "pcode" => $parcel->recipient_postal_code,
+            "email" => $parcel->recipient_email ?? null,
+            "phone" => $parcel->recipient_phone ?? null,
+            "contact" => $parcel->recipient_name ?? null,
+            "sender_remark" => $parcel->parcel_remark ?? null,
+            "weight" => !empty($parcel->parcel_weight) ? (float) $parcel->parcel_weight : null,
+            "num_of_parcel" => (int) $parcel->parcel_count,
+            "order_number" => $parcel->order_number ?? null,
+            "parcel_type" => $delivery_service,
+            "cod_amount" => !empty($parcel->cod_amount) ? (float) $parcel->cod_amount : null,
+            "cod_purpose" => $parcel->parcel_remark ?? null,
+            "pudo_id" => $parcel->location_id ? $location->location_id : null,
+            "predict" => 0,
+        ];
+
+        foreach ($additionalServicesIds as $additionalService) {
+            if ($additionalService == "INS") {
+                $payload['parcel_insurance'] = !empty($parcel->parcel_value) ? (float) $parcel->parcel_value : null;
+            }
+
+            if ($additionalService == "NOTIFY") {
+                $payload['predict'] = 1;
+            }
+        }
+
+        return $payload;
+    }
 
     protected function validateParcel($parcel)
     {
         $rules = [
-            'name1' => 'required|string|max:35',
-            'name2' => 'nullable|string|max:35',
-            'contact' => 'nullable|string|max:35',
-            'street' => 'required|string|max:35',
-            'rPropNum' => 'required|string|max:8',
-            'city' => 'required|string|max:35',
-            'pcode' => 'required|string|max:9|regex:/^[0-9]+$/',
-            'email' => 'nullable|email|max:50',
-            'phone' => 'nullable|string|max:30',
+            // Primatelj
+            'recipient_id' => 'nullable|string|max:100',
+            'recipient_name' => 'required|string|max:35',
+            'recipient_phone' => 'nullable|string|max:30',
+            'recipient_email' => 'nullable|email|max:50',
+            'recipient_adress' => 'required|string|max:70',
+            'recipient_city' => 'required|string|max:35',
+            'recipient_postal_code' => 'required|string|max:10',
+            'recipient_country' => 'required|string|size:2',
 
-            'sender_remark' => 'nullable|string|max:50',
-            'weight' => 'nullable|numeric|min:0.01',
-            'num_of_parcel' => 'required|integer|min:1',
+            // Paket
             'order_number' => 'nullable|string|max:20',
-            'order_number2' => 'nullable|string|max:20',
-            'parcel_type' => 'required|string|max:20',
-            'parcel_cod_type' => 'nullable|in:avg,all,firstonly',
+            'parcel_value' => 'nullable|numeric|min:0',
+            'parcel_weight' => 'nullable|numeric|min:0.01',
+            'parcel_remark' => 'nullable|string|max:50',
             'cod_amount' => 'nullable|numeric|min:0',
-            'cod_purpose' => 'nullable|string|max:14',
-            'predict' => 'nullable|in:1',
-            'return_of_document' => 'nullable|in:1',
-            'is_id_check' => 'nullable|in:1',
-            'id_check_receiver' => 'nullable|string|max:35',
-            'id_check_num' => 'nullable|string|max:5',
+            'cod_currency' => 'nullable|string|size:3',
+            'delivery_type' => 'nullable|string|in:Adresa,Paketomat',
+            'location_type' => 'nullable|string|in:Paketomat,Benza,Poštanski ured',
+            'location_id' => 'required_if:delivery_type,Paketomat|nullable|string|max:7',
+            'parcel_count' => 'required|integer|min:1',
+
+            // Pošiljatelj
+            'sender_id' => 'nullable|string|max:100',
             'sender_name' => 'nullable|string|max:30',
-            'sender_city' => 'nullable|string|max:30',
-            'sender_pcode' => 'nullable|string|max:9|regex:/^[0-9]+$/',
-            'sender_street' => 'nullable|string|max:30',
             'sender_phone' => 'nullable|string|max:20',
             'sender_email' => 'nullable|email|max:100',
-            'pudo_id' => 'nullable|string|max:7',
-            'dimension' => 'nullable|regex:/^[0-9]{1,3}[0-9]{1,3}[0-9]{1,3}$/',
-            'return_name' => 'nullable|string|max:35',
-            'return_name2' => 'nullable|string|max:35',
-            'return_street' => 'nullable|string|max:35',
-            'return_PropNum' => 'nullable|string|max:8',
-            'return_city' => 'nullable|string|max:35',
-            'return_pcode' => 'nullable|string|max:9|regex:/^[0-9]+$/',
-            'return_phone' => 'nullable|string|max:20',
+            'sender_adress' => 'nullable|string|max:60',
+            'sender_city' => 'nullable|string|max:30',
+            'sender_postal_code' => 'nullable|string|max:5',
+            'sender_country' => 'nullable|string|size:2',
+
+            // Kurir/usluge
+            'delivery_sevice' => 'required|string|max:20',
+            'delivery_additional_services' => 'nullable|string|max:255',
+            'parcel_size' => 'nullable|string|max:20',
+            'printer_type' => 'nullable|string|max:20',
+            'print_position' => 'nullable|integer|min:1|max:6',
+
+            // Dimenzije
+            'parcel_x' => 'nullable|integer|min:1|max:999',
+            'parcel_y' => 'nullable|integer|min:1|max:999',
+            'parcel_z' => 'nullable|integer|min:1|max:999',
+
+            // Reference
+            'parcel_ref_1' => 'nullable|string|max:35',
+            'parcel_ref_2' => 'nullable|string|max:35',
+            'parcel_ref_3' => 'nullable|string|max:35',
+
+            // Verifikacija/licenca
+            'username' => 'nullable|string|max:100',
+            'password' => 'nullable|string|max:100',
+            'client_number' => 'nullable|string|max:50',
+            'api_key' => 'nullable|string|max:200',
+            'domain' => 'nullable|string|max:255',
+            'licence' => 'nullable|string|max:255',
+            'email' => 'nullable|email|max:100',
         ];
 
         $messages = [
-            'name1.required' => 'Ime primatelja je obavezno',
-            'street.required' => 'Ulica primatelja je obavezna',
-            'rPropNum.required' => 'Kućni broj je obavezan',
-            'city.required' => 'Grad je obavezan',
-            'pcode.required' => 'Poštanski broj je obavezan',
-            'pcode.regex' => 'Poštanski broj smije sadržavati samo brojeve',
-            'email.email' => 'Email nije u ispravnom formatu',
-            'phone.max' => 'Telefon ne smije biti duži od 30 znakova',
-
-            'weight.numeric' => 'Težina mora biti broj',
-            'weight.min' => 'Težina mora biti veća od 0',
-
-            'num_of_parcel.required' => 'Broj paketa je obavezan',
-            'num_of_parcel.integer' => 'Broj paketa mora biti cijeli broj',
-            'num_of_parcel.min' => 'Broj paketa mora biti najmanje 1',
-
-            'parcel_type.required' => 'Vrsta paketa je obavezna',
-            'parcel_cod_type.in' => 'Nepoznata vrijednost za način raspodjele COD-a',
-            'cod_amount.numeric' => 'Iznos pouzeća mora biti broj',
-            'cod_amount.min' => 'Iznos pouzeća mora biti veći ili jednak 0',
-            'cod_purpose.max' => 'Referenca pouzeća ne smije biti duža od 14 znakova',
-            'predict.in' => 'Vrijednost za Predict mora biti 1',
-            'return_of_document.in' => 'Vrijednost za povrat dokumentacije mora biti 1',
-            'is_id_check.in' => 'Vrijednost za ID provjeru mora biti 1',
-            'dimension.regex' => 'Dimenzije moraju biti unijete kao niz bez razdjelnika (npr. 100110120)',
+            'recipient_name.required' => 'Ime primatelja je obavezno.',
+            'recipient_adress.required' => 'Adresa primatelja je obavezna.',
+            'recipient_city.required' => 'Grad primatelja je obavezan.',
+            'recipient_country.required' => 'Država primatelja je obavezna.',
+            'recipient_postal_code.required' => 'Poštanski broj je obavezan.',
+            'parcel_count.required' => 'Broj paketa je obavezan.',
+            'parcel_count.min' => 'Broj paketa mora biti najmanje 1.',
+            'delivery_type.required' => 'Način dostave je obavezan.',
+            'location_id.required_if' => 'Za dostavu u paketomat potrebno je poslati location_id.',
+            'delivery_sevice.required' => 'DPD usluga (npr. B2C/B2B) je obavezna.',
+            'parcel_weight.min' => 'Težina mora biti veća od 0.',
+            'parcel_remark.max' => 'Napomena može imati najviše 50 znakova.',
         ];
 
         $validator = Validator::make((array) $parcel, $rules, $messages);
@@ -494,5 +565,42 @@ class DpdController extends Controller
         return true;
     }
 
+    public function getParcelStatus(Request $request)
+    {
+        $requestBody = $request->getContent();
+        $jsonData = json_decode($requestBody);
 
+        $this->user = $jsonData->user;
+        $parcels = $jsonData->parcels;
+
+        $status_response = [];
+
+        foreach ($parcels as $parcel) {
+            $statusResponse = Http::withoutVerifying()
+                ->post(
+                    config('urls.hr.dpd') . "/parcel/parcel_status",
+                    [
+                        "secret" => "FcJyN7vU7WKPtUh7m1bx",
+                        "parcel_number" => $parcel->parcel_number,
+                    ]
+                );
+
+            $statusResponseJson = json_decode($statusResponse->body());
+
+            $status_response[] = [
+                "order_number" => $parcel->order_number,
+                "parcel_number" => $parcel->parcel_number,
+                "status_message" => $statusResponseJson->parcel_status,
+                "status_code" => "",
+                "status_date" => now()->format('Y-m-d\TH:i:s'),,
+                "color" => "#fff"
+            ];
+        }
+
+        return response()->json([
+            "data" => [
+                "statuses" => $status_response
+            ]
+        ], 201);
+    }
 }
