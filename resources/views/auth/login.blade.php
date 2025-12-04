d012e9983d03498d79f076411bd2474f0c1c77e6
Davor <davor@marsa.agency>
2025-12-04 17:13:20 +0100

    Update ContactFormMail to set reply-to address and modify login page title for improved user experience.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>@yield('title', 'Personal - Start Bootstrap Theme')</title>
+    <title>@yield('title', 'ExpressLabelMaker - Login')</title>
 
     <!-- Favicon-->
     <link rel="icon" type="image/x-icon" href="{{ asset('assets/favicon.ico') }}" />
