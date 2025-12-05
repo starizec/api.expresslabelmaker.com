7444d74e6a40fff9d55f84e4bc7e3c5a40e186a6
Davor <davor@marsa.agency>
2025-12-05 19:16:48 +0100

    Update GLS API URL in configuration for production and development environments to ensure correct endpoint usage.

--- config/urls.php
+++ config/urls.php
@@ -10,7 +10,7 @@ return (function () {
                 'overseas' => 'https://api.overseas.hr',
                 'hp-auth' => 'https://dxwebapi.posta.hr:9000/api',
                 'hp' => 'https://dxwebapi.posta.hr:9020/api',
-                'gls' => 'https://api.test.mygls.hr'
+                'gls' => 'https://api.mygls.hr'
             ],
             'si' => [
                 'dpd' => 'https://easyship.si/api'
@@ -19,7 +19,7 @@ return (function () {
         'development' => [
             'hr' => [
                 'dpd' => 'https://easyship.hr/api', 
-                'overseas' => 'https://apitest.overseas.hr',
+                'overseas' => 'https://api.overseas.hr',
                 'hp-auth' => 'https://dxwebapit.posta.hr:9000/api',
                 'hp' => 'https://dxwebapit.posta.hr:9020/api',
                 'gls' => 'https://api.test.mygls.hr'
